a202cd9c3f3a3036d57ebd0096164af64d72f50b
Ben Butterworth <24711048+ButterHub@users.noreply.github.com>
2020-08-23 15:25:20 +0100

    Merge all commits into one

--- app/src/main/java/org/tensorflow/lite/examples/posenet/CameraActivity.kt
+++ app/src/main/java/org/tensorflow/lite/examples/posenet/CameraActivity.kt
@@ -17,15 +17,15 @@
 package org.tensorflow.lite.examples.posenet
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class CameraActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.tfe_pn_activity_camera)
-    savedInstanceState ?: supportFragmentManager.beginTransaction()
-      .replace(R.id.container, PosenetActivity())
-      .commit()
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tfe_pn_activity_camera)
+        savedInstanceState ?: supportFragmentManager.beginTransaction()
+            .replace(R.id.container, PosenetFragment()).commit()
+    }
 }
